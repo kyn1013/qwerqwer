ea86e1802134e6d213d3ee30356b6cd6521bf50c
kyn1013 <yn112511@naver.com>
2024-10-06 17:35:33 +0900

    Add : hello class 변경

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("마스터에서 실행");
+        System.out.println("예나 브랜치에서 추가한 내용");
     }
 }
