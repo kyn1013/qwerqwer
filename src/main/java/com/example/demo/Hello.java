a6a514a2800f6b52da3b365352abc4b751ea8e60
kyn1013 <yn112511@naver.com>
2024-10-06 18:02:00 +0900

    33

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -3,5 +3,7 @@ package com.example.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("저는 이렇게 바꾸고 싶어요");
+        System.out.println("저는 이렇게 바꾸고 싶어요2");
     }
+
 }
