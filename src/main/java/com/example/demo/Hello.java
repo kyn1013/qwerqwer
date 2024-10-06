f5ce7dc42ab740e919f97ee370dbc2a840f55f79
kyn1013 <yn112511@naver.com>
2024-10-06 18:48:04 +0900

    Fix:3

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("저는 이렇게 바꾸고 싶어요");
         System.out.println("저는 이렇게 바꾸고 싶어요2");
+        System.out.println("저는 이렇게 바꾸고 싶어요3");
     }
 
 }
